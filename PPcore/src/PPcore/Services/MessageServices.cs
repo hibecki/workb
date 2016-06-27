7d34605f853623f3a172c73f82ff80527da52277
hibecki <b17032016@gmail.com>
2016-06-27 15:17:35 +0700

    tyyyyyyyy

--- PPcore/src/PPcore/Services/MessageServices.cs
+++ PPcore/src/PPcore/Services/MessageServices.cs
@@ -14,17 +14,28 @@ namespace PPcore.Services
             try
             {
                 MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient("mail.palangpanya.com");
 
-                mail.From = new MailAddress("info@palangpanya.com");
+                //SmtpClient SmtpServer = new SmtpClient("mail.palangpanya.com");
+                //mail.From = new MailAddress("info@palangpanya.com");
+                //mail.To.Add(email);
+                //mail.Subject = subject;
+                //mail.Body = message;
+                //SmtpServer.Port = 25;
+                //SmtpServer.Credentials = new System.Net.NetworkCredential("info@palangpanya.com", "mfmHD9A2Ws");
+                //SmtpServer.EnableSsl = false;
+
+                SmtpClient SmtpServer = new SmtpClient("smtp-mail.outlook.com");
+                mail.From = new MailAddress("b170320162016@outlook.com");
                 mail.To.Add(email);
                 mail.Subject = subject;
                 mail.Body = message;
-                SmtpServer.Port = 25;
+                //SmtpServer.Port = 25;
                 //SmtpServer.Port = 465;
-                //SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("info@palangpanya.com", "mfmHD9A2Ws");
-                SmtpServer.EnableSsl = false;
+                SmtpServer.Port = 587;
+                SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("b170320162016@outlook.com", "pp27062016");
+                SmtpServer.EnableSsl = true;
 
                 SmtpServer.Send(mail);
                 res = 0;
