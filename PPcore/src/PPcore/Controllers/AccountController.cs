3ab83b5ead9714dbce620648b75335554cb15460
hibecki <b17032016@gmail.com>
2016-07-14 15:23:07 +0700

    fffff

--- PPcore/src/PPcore/Controllers/AccountController.cs
+++ PPcore/src/PPcore/Controllers/AccountController.cs
@@ -7,6 +7,9 @@ using PPcore.Models;
 using PPcore.Services;
 using Microsoft.EntityFrameworkCore;
 using System.Data.SqlClient;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
 
 namespace PPcore.Controllers
 {
@@ -14,11 +17,15 @@ namespace PPcore.Controllers
     {
         private readonly PalangPanyaDBContext _context;
         private readonly IEmailSender _emailSender;
+        private IConfiguration _configuration;
+        private IHostingEnvironment _env;
 
-        public AccountController(PalangPanyaDBContext context, IEmailSender emailSender)
+        public AccountController(PalangPanyaDBContext context, IEmailSender emailSender, IConfiguration configuration, IHostingEnvironment env)
         {
             _context = context;
             _emailSender = emailSender;
+            _configuration = configuration;
+            _env = env;
         }
 
         public void SendEmail(string email, string username, string password)
@@ -37,7 +44,52 @@ namespace PPcore.Controllers
             string password = cid_card.Substring(cid_card.Length - 4);
             try
             {
-                _context.Database.ExecuteSqlCommand("INSERT INTO member (member_code,cid_card,birthdate,fname,lname,mobile,email,x_status,mem_password) VALUES ('" + cid_card + "','" + cid_card + "','" + birthdate + "',N'" + fname + "',N'" + lname + "','" + mobile + "','" + email + "','Y','" + password + "')");
+                if ((!string.IsNullOrEmpty(mem_photo)) && (mem_photo.Substring(0, 1) != "M"))
+                {
+                    var fileName = mem_photo.Substring(9);
+                    var fileExt = Path.GetExtension(fileName);
+                    pic_image m = new pic_image();
+                    m.image_code = "M" + DateTime.Now.ToString("yyMMddhhmmssfffffff") + fileExt;
+                    m.x_status = "Y";
+                    m.image_name = fileName;
+
+                    m.ref_doc_type = "member";
+                    m.ref_doc_code = cid_card; //member_code;
+                    fileName = m.image_code;
+                    _context.pic_image.Add(m);
+                    _context.SaveChanges();
+
+                    var s = Path.Combine(Path.Combine(_env.WebRootPath, _configuration.GetSection("Paths").GetSection("images_upload").Value), mem_photo);
+                    var d = Path.Combine(Path.Combine(_env.WebRootPath, _configuration.GetSection("Paths").GetSection("images_member").Value), fileName);
+                    System.IO.File.Copy(s, d, true);
+                    System.IO.File.Delete(s);
+                    //clearImageUpload();
+
+                    mem_photo = m.image_code;
+                }
+                if ((!string.IsNullOrEmpty(cid_card_pic)) && (cid_card_pic.Substring(0, 1) != "C"))
+                {
+                    var fileName = cid_card_pic.Substring(9);
+                    var fileExt = Path.GetExtension(fileName);
+                    pic_image pic_image = new pic_image();
+                    pic_image.image_code = "C" + DateTime.Now.ToString("yyMMddhhmmssfffffff") + fileExt;
+                    pic_image.x_status = "Y";
+                    pic_image.image_name = fileName;
+                    pic_image.ref_doc_type = "cidcard";
+                    pic_image.ref_doc_code = cid_card; //member_code;
+                    fileName = pic_image.image_code;
+                    _context.pic_image.Add(pic_image);
+                    _context.SaveChanges();
+
+                    var s = Path.Combine(Path.Combine(_env.WebRootPath, _configuration.GetSection("Paths").GetSection("images_upload").Value), cid_card_pic);
+                    var d = Path.Combine(Path.Combine(_env.WebRootPath, _configuration.GetSection("Paths").GetSection("images_member").Value), fileName);
+                    System.IO.File.Copy(s, d, true);
+                    System.IO.File.Delete(s);
+                    //clearImageUpload();
+
+                    cid_card_pic = pic_image.image_code;
+                }
+                _context.Database.ExecuteSqlCommand("INSERT INTO member (member_code,cid_card,birthdate,fname,lname,mobile,email,x_status,mem_password,mem_photo,cid_card_pic) VALUES ('" + cid_card + "','" + cid_card + "','" + birthdate + "',N'" + fname + "',N'" + lname + "','" + mobile + "','" + email + "','Y','" + password + "','" + mem_photo + "','" + cid_card_pic + "')");
                 SendEmail(email, cid_card, password);
             }
             catch (SqlException ex)
