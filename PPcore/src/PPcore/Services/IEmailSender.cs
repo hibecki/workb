50e90cf92b48af52364499d3ad9340752799e77f
hibecki <b17032016@gmail.com>
2016-07-30 21:22:15 +0700

    67777777

--- PPcore/src/PPcore/Services/IEmailSender.cs
+++ PPcore/src/PPcore/Services/IEmailSender.cs
@@ -8,5 +8,6 @@ namespace PPcore.Services
     public interface IEmailSender
     {
         Task SendEmailAsync(string email, string subject, string message);
+        Task SendEmailWithAttachment(string email, string subject, string message, List<string> attachedPath);
     }
 }
--- PPcore/src/PPcore/Services/MessageServices.cs
+++ PPcore/src/PPcore/Services/MessageServices.cs
@@ -38,5 +38,40 @@ namespace PPcore.Services
 
             return Task.FromResult(res);
         }
+
+        public Task SendEmailWithAttachment(string email, string subject, string message, List<string> attachedPath)
+        {
+            int res = -1;
+            try
+            {
+                using (var msg = new MailMessage("info@palangpanya.com", email))
+                {
+                    msg.Subject = subject;
+                    msg.Body = message;
+                    foreach (string s in attachedPath)
+                    {
+                        msg.Attachments.Add(new Attachment(s));
+                    }
+                    using (SmtpClient client = new SmtpClient
+                    {
+                        EnableSsl = false,
+                        Host = "mail.palangpanya.com",
+                        Port = 25,
+                        Credentials = new NetworkCredential("info@palangpanya.com", "mfmHD9A2Ws")
+                    })
+                    {
+                        client.Send(msg);
+                        res = 0;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                res = e.HResult;
+            }
+
+
+            return Task.FromResult(res);
+        }
     }
 }
